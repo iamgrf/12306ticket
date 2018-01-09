910fd37f118775a874be792784f1f5f679c1ebb6
xiangy <1513330485@qq.com>
2018-01-09 18:09:40 +0800

    login

--- src/main/java/com/ticket/Test.java
+++ src/main/java/com/ticket/Test.java
@@ -1,13 +1,9 @@
 package com.ticket;
 
-import com.ticket.util.Constant;
-
-import java.util.List;
-import java.util.Random;
-
 public class Test {
 
     public static void main(String[] args) {
+        String msg = "L4hoWs8aEoLTThBX9tZjWl0SOitZ8i%2BglNTFjxkAdTOnC7mNn5v1US7aYWKvk%2BajYnWMEoIkNKZ3%0A4atOvqghT7c6A%2FKK4XCvWBnGiAXY8A%2FgML93pJR7YWbRVIU0MMYwkpWblDc3G5SxOhgd4%2FxFx%2BY2%0AK1hYkQl2vm4TN5NW5UJXvAmAsY2IrjBfBIkhJIOIOA4JX5DKiw%2BPKSxgiKER4OTrQx7mIVF%2FKnae%0Ax5Y37PrArZaIQQrx742as3iC3%2BGREE9bBRkBTwM%3D";
 
     }
 
